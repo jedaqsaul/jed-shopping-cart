9ff18aaa4a4e7ddb4177c59f04992d6839973f90
jedidiah <jedaqsaul@gmail.com>
2025-04-08 13:30:47 +0300

    Add products to the cart

--- main.js
+++ main.js
@@ -27,7 +27,36 @@ function fetchProducts() {
         
           
           `;
+        // grab the button inside this div and attach the event listener
+        const addButton = productDiv.querySelector(".add-to-cart");
+        addButton.addEventListener("click", () => {
+          addToCart(product.id);
+        });
         productSection.appendChild(productDiv);
       });
+    })
+    .catch((err) => {
+      console.error("Error fetching products: ", err);
+    });
+}
+
+function addToCart(productId) {
+  fetch(`${BASE_URL}/products/${productId}`)
+    .then((res) => res.json())
+    .then((product) => {
+      fetch(`${BASE_URL}/cart`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+      })
+        .then((res) => res.json())
+        .then((cartItem) => {
+          alert(`${cartItem.name} added to the cart!`);
+        });
+    })
+    .catch((err) => {
+      console.error("Error adding to the cart", err);
     });
 }
